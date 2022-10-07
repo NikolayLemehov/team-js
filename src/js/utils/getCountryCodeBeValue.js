76368eba84da95a7948c3038a09f6b31ca17d4cb
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-10-07 15:45:55 +0300

    rename getCountryCodeBeValue.js to getCountryListBeValue.js

--- src/js/utils/getCountryCodeBeValue.js
+++ src/js/utils/getCountryCodeBeValue.js
@@ -1,6 +1,6 @@
 import {countries} from "../variables/countries";
 
 
-export function getCountryCodeBeValue (value) {
+export function getCountryListBeValue (value) {
   return countries.filter(it => it.name.includes(value) || it.code.toLowerCase().includes(value))
 }
