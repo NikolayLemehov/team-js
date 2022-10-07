ca231955edee672521848d68e2a3826e72dcf573
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-10-07 15:58:01 +0300

    modal-ver2.1

---
+++ src/js/modal.js
@@ -0,0 +1,13 @@
+let more = document.querySelector('.more');
+modal = document.querySelector('.modal');
+close = document.querySelector('.close');
+
+more.addEventListener('click', function () {
+  modal.style.display = 'block';
+  modal.classList.add('modal-animation');
+});
+
+close.addEventListener('click', function () {
+  modal.style.display = 'none';
+  modal.classList.remove('modal-animation');
+});
