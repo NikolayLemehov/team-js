e253724483f7afcf5992bd6ac4f19d5c6afd962b
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-10-08 22:13:47 +0300

    Add markup

--- src/js/markup/getCardMarkup.js
+++ src/js/markup/getCardMarkup.js
@@ -1,3 +1,18 @@
 export function getCardMarkup(data) {
-  return `<div>cardMarkup</div>`
+  return `<li class="card__item" data-id="${data.id}">
+          <a href="${data.url}" class="card__link">
+            <div class="card__thumb">
+              <img src="${data.images[4].url}" alt="card">
+            </div>
+          </a>
+          <div class="event__info">
+            <h2 class="event__artist">${data.name}</h2>
+            <p class="event__data">${data.dates.localDate}</p>
+            <a class="event__address" href="">
+              <svg class="event__icon">
+                <use href="./images/symbol-defs.svg#locationVector">
+                </use>
+              </svg>${data._embedded.venues[0].name}</a>
+          </div>
+        </li>`
 }
