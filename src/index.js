4ba34fa8219e617ec9308d5b1fc74d2eaef64e1a
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-10-09 16:09:31 +0300

    fix first fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './js/module/eventlist';
 import './js/module/pagination';
 import './js/module/searchForm';
+import {renderInit} from "./js/module/renderInit";
+
+renderInit().catch(console.log)
--- src/js/module/renderEventList.js
+++ src/js/module/renderEventList.js
@@ -1,10 +1,7 @@
 import {removeChildren} from "../utils/removeChildren";
 import { getCardListMarkup } from "../markup/getCardListMarkup";
-import { eventApi } from "../api/EventApi";
 
 const cardListRef = document.querySelector('.card-list');
-
-eventApi.fetchEvents().then(e => renderEventList(e.data._embedded.events));
 
 export function renderEventList(data) {
   removeChildren(cardListRef);
