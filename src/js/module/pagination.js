111762fcb80f45bc11d01ca92a3de7d6979f0506
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-10-10 12:57:40 +0300

    add pagination changes

--- src/js/module/pagination.js
+++ src/js/module/pagination.js
@@ -5,7 +5,7 @@ import { removeChildren } from '../utils/removeChildren';
 import { renderEventList } from './renderEventList';
 import { getEvents } from '../selectors/getEvents';
 
-const paginationBoxRef = document.querySelector('.pagination-box');
+const paginationBoxRef = document.querySelector('.pagination-box__list');
 
 paginationBoxRef.addEventListener('click', onPaginationBoxClick);
 
