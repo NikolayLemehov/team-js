a388936e179090ab6bec008692aaca92834a4dda
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-10-12 19:54:30 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './js/module/select';
-import './js/module/eventlist';
-import './js/module/pagination';
-import './js/module/searchForm';
-import './js/module/preloader';
+// import './js/module/select';
+// import './js/module/eventlist';
+// import './js/module/pagination';
+// import './js/module/searchForm';
+// import './js/module/preloader';
 import './test';
-import { renderEventsWithPagination } from './js/module/renderEventsWithPagination';
-import { Notify } from 'notiflix';
+// import { renderEventsWithPagination } from './js/module/renderEventsWithPagination';
+// import { Notify } from 'notiflix';
 
-renderEventsWithPagination().catch(e => Notify.failure(e.message));
+// renderEventsWithPagination().catch(e => Notify.failure(e.message));
--- src/test.js
+++ src/test.js
@@ -1,47 +1,65 @@
-const img = document.querySelector('.test-img1')
-const img2 = document.querySelector('.test-img2')
-const img3 = document.querySelector('.test-img3')
-console.log('img', img.complete)
-function loaded(num) {
-  console.trace(num)
-}
-loadImg(img, 1).then(onSuccess).catch(onError)
-loadImg(img2, 2).then(console.log)
-loadImg(img3, 3).then(console.log)
+import defaultImg from './images/hero_bg@1x_mob.jpg'
 
-function loadImg(img) {
-  return new Promise((res, rej) => {
-    img.addEventListener('load', () => res(img), {once: true});
-    img.addEventListener('error', (err) => rej(img), {once: true});
-  })
-}
+const label = 'test';
+console.time(label);
 
-function onSuccess(img) {
-  console.log('img success', img.complete)
-  img.src = img.dataset.src
-}
-
-function onError(img) {
-  console.log('img error', img.complete)
-  img.src = 'default url'
-}
+// const img = document.querySelector('.test-img1')
+// const img2 = document.querySelector('.test-img2')
+// const img3 = document.querySelector('.test-img3')
+// // console.log('img', img.complete)
+// function loaded(num) {
+//   console.trace(num)
+// }
+// // loadImg(img, 1).then(onSuccess).catch(onError)
+// // loadImg(img2, 2).then(console.log)
+// // loadImg(img3, 3).then(console.log)
+//
+// function loadImg(img) {
+//   return new Promise((res, rej) => {
+//     img.addEventListener('load', () => res(img), {once: true});
+//     img.addEventListener('error', (err) => rej(img), {once: true});
+//   })
+// }
+//
+// function onSuccess(img) {
+//   console.log('img success', img.complete)
+//   img.src = img.dataset.src
+// }
+//
+// function onError(img) {
+//   console.log('img error', img.complete)
+//   img.src = 'default url'
+// }
 
 const imgSmall = document.querySelectorAll('.loading');
 
 const loadImage = () => {
   for (let i = 0; i < imgSmall.length; i++) {
     const imgLarge = new Image();
+    imgLarge.dataset.largeNew = '';
     imgLarge.src = imgSmall[i].dataset.large;
+    console.log('add src');
+    console.timeLog(label);
+
     imgLarge.addEventListener('load', () => {
+
+      console.log('add load');
+      console.timeLog(label);
       imgLarge.classList.add('onload');
+
       setTimeout(
         () => {
+          console.log('timeout');
+          console.timeLog(label);
           imgSmall[i].classList.add('onload');
         }, 650);
+
+      imgSmall[i].parentNode.appendChild(imgLarge);
     }, false);
-    imgSmall[i].parentNode.appendChild(imgLarge);
+    console.log('append');
+    console.timeLog(label);
   }
-}
+};
 
 loadImage();
 
