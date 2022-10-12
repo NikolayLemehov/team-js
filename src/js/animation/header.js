db0c1b7487f745d2b60d031633b18c0ef2a898e1
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-10-12 19:23:23 +0300

    part 1

---
+++ src/js/animation/header.js
@@ -0,0 +1,17 @@
+import anime from 'animejs';
+
+anime({
+  targets: '.first-part',
+  translateX: 100,
+  //   rotate: '1turn',
+  duration: 3000,
+  //   easing: 'spring(1, 80, 10, 0)',
+});
+
+anime({
+  targets: '.second-part',
+  translateX: 100,
+  //   rotate: '1turn',
+  duration: 4000,
+  //   easing: 'spring(1, 80, 10, 0)',
+});
