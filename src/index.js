c350350ac24fd9824a1bb72ea0734a07eaf453ca
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-10-12 19:48:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import './js/module/select';
 import './js/module/eventlist';
 import './js/module/pagination';
 import './js/module/searchForm';
-<<<<<<< HEAD
 import './js/animation/header';
-=======
 import './js/module/preloader';
->>>>>>> dev
 import { renderEventsWithPagination } from './js/module/renderEventsWithPagination';
 import { Notify } from 'notiflix';
 
